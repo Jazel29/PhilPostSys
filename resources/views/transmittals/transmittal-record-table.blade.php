4e55ff7fd1fd68beefef104ee37d988e0f1220b0
hwaiatt <134309186+hwaiatt@users.noreply.github.com>
2024-02-19 11:11:44 +0800

    updated transmittals record page

--- resources/views/transmittals/transmittal-record-table.blade.php
+++ resources/views/transmittals/transmittal-record-table.blade.php
@@ -75,25 +75,19 @@
 </style>
 
 <div class="container">
-    <div class="d-flex">
-        <div class="row mt-3">
+    <div class="row mt-3">
+        <div class="col">
             <h1 class="display-6">Transmittal Record</h1>
         </div>
-    </div>
-    
-    <div class="row text-right">
-        <div class="col">
+        <div class="col text-right">
             <button class="btn btn-outline-success" onclick="exportToExcel()">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-excel-fill" viewBox="0 0 16 16">
-                    <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0M9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1M5.884 6.68 8 9.219l2.116-2.54a.5.5 0 1 1 .768.641L8.651 10l2.233 2.68a.5.5 0 0 1-.768.64L8 10.781l-2.116 2.54a.5.5 0 0 1-.768-.641L7.349 10 5.116 7.32a.5.5 0 1 1 .768-.64"/>
-                </svg>
+            &nbsp;<i class="fa-solid fa-table"></i> &nbsp; Export as Excel
             </button>
-        </div>
-    </div>
 </div>
 
     <!-- Left side column -->
     <div class="row mt-3">
+        
     <!-- Left side column -->
     <div class="col-md-4">
         <div class="rounded-container">
@@ -157,6 +151,7 @@
         </div>
     </div>
 </div>
+ 
 <script>
     function exportToExcel() {
         console.log('exportToExcel called');
