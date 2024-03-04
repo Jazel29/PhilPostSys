13337077b6f7577bda0915d329dae228c64fbe15
0roc4n <esparragojames@gmail.com>
2024-03-04 16:00:18 +0800

    fix transmittals update

--- resources/views/transmittals/edit-transmittals-form.blade.php
+++ resources/views/transmittals/edit-transmittals-form.blade.php
@@ -758,11 +758,22 @@
                 </div>
 
                 <div class="row mt-2">
-                    <input class="form-control rounded-md text-19 input-border" list="datalistOptions" value="{{ old('addresseeDataList', $addressee->abbrev . ' - ' . $addressee->name_primary) }}" id="addresseeDataList" placeholder="Addressee"required>
+                    <input class="form-control rounded-md text-19 input-border" list="datalistOptions" id="addresseeDataList" placeholder="Addressee" value="{{ old('addresseeDataList', $addressee->abbrev . ' - ' . $addressee->name_primary) }}" required>
                     <datalist id="datalistOptions">
                         <option value="Add New Addressee"></option>
                     </datalist>
                     <input class="form-control" type="hidden" name="receiver" id="receiver">
+                </div>
+                <div id="popover-content" class="mt-2 text-danger" style="display: none;">
+                    Invalid Addressee. <a href="#" onclick="openModal()" class="underline-link">Click here</a> to add new addressee.
+                </div>
+                <div class="row mt-4">
+                   <div class="text-gray-500">
+                        Address:
+                   </div> 
+                    <textarea id="address" name="address" rows="2" class="rounded-md text-19" style="border-color:#a0aec0;">
+                    {{ old('addresseeDataList', $addressee->zip . '-' . $addressee->city . ' - ' . $addressee->province) }}
+                    </textarea>
                 </div>
             </div>
         </div>
@@ -906,6 +917,48 @@
                 <button type="submit" class="btn btn-primary">Save changes</button>
                 </form>
             </div>
+        </div>
+    </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="newAddresseeModal" tabindex="-1" role="dialog" aria-labelledby="newAddresseeModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="newAddresseeModalLabel">Add New Addressee</h5>
+            </div>
+            <div class="mssg">
+                @if(session('record_added'))
+                
+                    <div class="alert alert-primary" role="alert">
+                        <p>{{ session('record_added') }}</p>
+                    </div>
+                    <script>
+                        $(document).ready(function () {
+                            $('#newAddresseeModal').modal('show');
+                        });
+                    </script>
+                @endif
+            </div>
+            <form action="/add_addressee" method="post">
+                @csrf
+                <div class="modal-body">
+                    <!-- Add your form fields for adding a new addressee here -->
+                    <!-- Example: -->
+                    <input type="text" name="nameAbbrev" id="nameAbbrev" class="form-control mb-2" placeholder="Addressee Abbreviation" required>
+                    <input type="text" name="namePrimary" id="namePrimary" class="form-control mb-2" placeholder="Addressee Name Line 1" required>
+                    <input type="text" name="nameSecondary" id="nameSecondary" class="form-control mb-2" placeholder="Addressee Name Line 2">
+                    <input type="text" name="address" id="address" class="form-control mb-2" placeholder="Floor/Bldg/Street/Barangay ">
+                    <input type="text" name="city" id="city" class="form-control mb-2" placeholder="City/Municipality" required>
+                    <input type="text" name="zip" id="zip" class="form-control mb-2" placeholder="Zip Code" required>
+                    <input type="text" name="province" id="province" class="form-control mb-2" placeholder="Province"   required>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-outline-secondary" onclick="closeModal()">Close</button>
+                    <button type="submit" class="btn btn-outline-primary" onclick="saveAddresee()">Save Addressee</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
@@ -1059,87 +1112,11 @@
         $('#example').dataTable();
 
     } );
-    var rrr_tns = [];
-    var count = 0;
 
-    function removeTN(element, rrr_tn_value) {
-        // Remove the container from the DOM
-        element.parentNode.removeChild(element);
-
-        // Remove the corresponding rrr_tn_value from the rrr_tns array
-        var index = rrr_tns.indexOf(rrr_tn_value);
-        if (index !== -1) {
-            rrr_tns.splice(index, 1);
-        }
-
-        // Log the updated rrr_tns array (for testing purposes)
-        console.log(rrr_tns);
-        updateCounts();
-    }
-
-    function updateCounts() {
-    // Get all tn_containers in the rrr_div
-        var tnContainers = document.getElementById('rrr_div').getElementsByClassName('container');
-
-        // Update the counts based on the current index in the rrr_tns array
-        for (var i = 0; i < tnContainers.length; i++) {
-            var countElement = tnContainers[i].getElementsByTagName('p')[0];
-            countElement.innerText = (i + 1) + '. ' + rrr_tns[i];
-        }
-    }
-
-    function addTN() {
-        var rrr_tn_value = document.getElementById('rrr_tn').value;
-
-        // Check if rrr_tn_value is truthy before appending
-        if (rrr_tn_value) {
-            // Add the rrr_tn_value to the rrr_tns array
-            rrr_tns.push(rrr_tn_value);
-
-            // Get the current index in the rrr_tns array
-            var count = rrr_tns.length;
-
-            // Create a new tn_container with the extracted value
-            var tn_container = document.createElement('div');
-            tn_container.className = 'container';
-            tn_container.innerHTML = '<span class="exit-button" onclick="removeTN(this.parentNode, \'' + rrr_tn_value + '\')">✖</span><p>' + count + ". " + rrr_tn_value + '</p>';
-
-            // Append the new tn_container to the rrr_div
-            document.getElementById('rrr_div').appendChild(tn_container);
-
-            // Clear the value of rrr_tn input field
-            document.getElementById('rrr_tn').value = '';
-
-            // Log the updated rrr_tns array (for testing purposes)
-            console.log(rrr_tns);
-        }
-    }
-
-    var rrr_tn_value = document.getElementById("rrr_tn");
-    rrr_tn_value.addEventListener("keypress", function(event) {
-        if (event.key === "Enter") {
-            event.preventDefault();
-            addTN();
-        }
-    });
-
-    var mailTn = document.getElementById("mail_tn");
-    mailTn.addEventListener("keypress", function(evt) {
-        if (event.key === "Enter") {
-            evt.preventDefault();
-        }
-    });
-
-    function submitForm() {
-
-        // Set the rrr_tns array value to the hidden input
-        document.getElementById('rrr_tns_input').value = JSON.stringify(rrr_tns);
-
-        // Submit the form
-        document.forms[0].submit();
-    }
-
-    document.getElementById('addresseeDataList').addEventListener('input', function() {
+    
+    document.getElementById('addresseeDataList').addEventListener('input', handleAddresseeDataListInput);
+    document.addEventListener('DOMContentLoaded', fetchAddressees);
+    function handleAddresseeDataListInput() {
         var addressValue = document.getElementById('address');
         var addresseeVal = document.getElementById('receiver');
         var RRRdiv = document.getElementById('addRRR_div');
@@ -1151,14 +1128,12 @@
             $('#newAddresseeModal').modal('show');
             this.value = '';
         } else {
-            // Get the selected option element
             var selectedOption = document.querySelector('#datalistOptions option[value="' + selectedValue + '"]');
-            
-            // Check if the selected option exists
+
             if (selectedOption) {
-                selectedId = selectedOption.id;
-                var selectedAddressee = addressees.find(addressee => addressee.id == selectedId)
-                addressValue.value = selectedAddressee.address + " " + selectedAddressee.city + " " + selectedAddressee.zip + " " + selectedAddressee.province;
+                var selectedId = selectedOption.id;
+                var selectedAddressee = addressees.find(addressee => addressee.id == selectedId);
+                addressValue.value = `${selectedAddressee.address} ${selectedAddressee.city} ${selectedAddressee.zip} ${selectedAddressee.province}`;
                 addresseeVal.value = selectedId;
                 RRRdiv.style.display = 'block';
                 popUp.style.display = 'none';
@@ -1166,18 +1141,16 @@
                 addressValue.value = '';
                 RRRdiv.style.display = 'none';
                 popUp.style.display = 'block';
-
             }
-            
-            if(!selectedOption & selectedValue == '') {
+
+            if (!selectedOption & selectedValue == '') {
                 popUp.style.display = 'none';
             }
         }
-    });
-
-
+    }
     function closeModal() {
         $('#newAddresseeModal').modal('hide');
+        $('#confirmationModal').modal('hide');
     }
 
     function openModal() {
@@ -1188,45 +1161,33 @@
         $('#newAddresseeModal').modal('hide');
     }
 
-    document.addEventListener('DOMContentLoaded', function () {
-        // Fetch addressees from the server
+    function fetchAddressees() {
         fetch('/get-addressees')
             .then(response => response.json())
-            .then(data => {
-                const datalist = document.getElementById('datalistOptions');
-                addressees = data.addressees; // Store addressees globally for later access
-
-                addressees.forEach(addressee => {
-                    const option = document.createElement('option');
-                    option.value = `${addressee.abbrev} - ${addressee.name_primary}`;
-                    option.id = addressee.id;
-                    datalist.appendChild(option);
-                });
-            })
+            .then(populateAddresseesDatalist)
             .catch(error => console.error('Error fetching addressees:', error));
-    });
+    }
 
-    $(document).ready(function () {
-        $('#mail_tn').on('blur', function () {
-            var mail_tn = $(this).val();
+    function populateAddresseesDatalist(data) {
+        const datalist = document.getElementById('datalistOptions');
+        addressees = data.addressees;
 
-            // Perform AJAX request to check existence
-            $.ajax({
-                type: 'GET',
-                url: '/checkMailTN', // Replace with your actual route
-                data: { mail_tn: mail_tn },
-                success: function (response) {
-                    if (response.exists) {
-                        $('#mail_tn_error').text('Mail Tracking Number already exists');
-                    } else {
-                        $('#mail_tn_error').text('');
-                    }
-                },
-                error: function (error) {
-                    console.error('Error checking Mail Tracking Number:', error);
-                }
-            });
+        addressees.forEach(addressee => {
+            const option = document.createElement('option');
+            option.value = `${addressee.abbrev} - ${addressee.name_primary}`;
+            option.id = addressee.id;
+            datalist.appendChild(option);
         });
-    });
+    }
+
+    function submitForm() {
+
+        // Set the rrr_tns array value to the hidden input
+        document.getElementById('rrr_tns_input').value = JSON.stringify(rrr_tns);
+
+        // Submit the form
+        document.forms[0].submit();
+    }
+
 
 </script>
